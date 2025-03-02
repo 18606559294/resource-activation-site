b874635c6126fb40f62397c63ebf35fc42eb3382
18606559294 <73704827+18606559294@users.noreply.github.com>
2025-03-02 11:54:01 +0000

    Deploying to gh-pages from @ 18606559294/resource-activation-site@f7518fd8ddad17380b49a7265f643d7469e87c59 🚀

---
+++ assets/api-keys-BI2k6KOc.js
@@ -0,0 +1,2 @@
+const E={DEEPSEEK_API_KEY:"sk-718a2c0f3a8843209cb8eb54529cfba2",DEEPSEEK_API_KEY_ENCRYPTED:null,ENCRYPTION_KEY:"resource-activation-site-secure-key"};export{E as default};
+//# sourceMappingURL=api-keys-BI2k6KOc.js.map
---
+++ assets/deepseek-chat-init-z77UTMhl.js
@@ -0,0 +1,2 @@
+import{D as e,i as t}from"./ad-manager-nkcXdfJJ.js";document.addEventListener("DOMContentLoaded",(()=>{setTimeout((()=>{try{window.deepseekChatWidget=new e,console.log(t.t("deepseek.logs.initialized","DeepSeek客服聊天窗口已加载"))}catch(o){console.error(t.t("deepseek.logs.initFailed","初始化DeepSeek客服聊天窗口失败:"),o)}}),1e3)}));
+//# sourceMappingURL=deepseek-chat-init-z77UTMhl.js.map
